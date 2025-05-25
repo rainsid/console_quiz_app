ac81481acfb4863cb55c9a04c9c22f61a4c6626a
rainsid <adrianroycidro@gmail.com>
2025-05-25 22:46:57 +0800

    fix view all users

--- src/view/AppView.java
+++ src/view/AppView.java
@@ -1,216 +1,224 @@
 package view;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import ConsoleColor.TerminalColors;
+import helper.ConsoleUtil;
+import helper.Banner;
 
 public class AppView {
-    private Scanner sc = new Scanner(System.in);
-    private TerminalColors tc;
+  private Scanner sc = new Scanner(System.in);
+  private TerminalColors tc;
 
-    public int showMainMenu() {
-        System.out.println();
-        System.out.println(tc.BLUE + tc.WHITE_BG + "Main Menu: " + tc.RESET);
-        System.out.println("1. Admin Login👨🏻");
-        System.out.println("2. User Registration");
-        System.out.println("3. User Login");
-        System.out.println("4. Exit");
-        return getIntInput("Enter choice: ");
+  public int showMainMenu() {
+    ConsoleUtil.clearConsole();
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println("1. Admin Login 👩‍💼💻🔑 ");
+    System.out.println("2. User Login 👤💻");
+    System.out.println("3. Exit 👋😊");
+    return getIntInput("Enter the number of your choice");
+  }
+
+  // --------- admin methods ----------------
+  public String enterAdminUsername() {
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println(Banner.ADMIN_LOGIN);
+    return getStringInput("Enter admin username: ");
+  }
+
+  public String getAdminPassword() {
+    return getStringInput("Enter admin password: ");
+  }
+
+  // -------------- admin menu ------------------------
+  public int showAdminMenu() {
+    ConsoleUtil.clearConsole();
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println(Banner.ADMIN_MENU);
+    System.out.println("1. Manage Users");
+    System.out.println("2. Manage Quiz");
+    System.out.println("3. Go back to Main Menu");
+    return getIntInput("Enter choice: ");
+  }
+
+  // ----------- User management ----------------
+  public int showManageUserMenu() {
+    ConsoleUtil.clearConsole();
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println(Banner.MANAGE_USER);
+    System.out.println("1. Add User");
+    System.out.println("2. View All Users");
+    System.out.println("3. View a User");
+    System.out.println("4. Update User");
+    System.out.println("5. Delete User");
+    System.out.println("6. Go back to Admin Menu");
+    return getIntInput("Enter number of your choice");
+  }
+
+  // ------------- input username and last name user -----------------
+  public void inputUserInfo() {
+    String firstname = getStringInput("Enter first name: ");
+    String lastname = getStringInput("Enter last name: ");
+  }
+
+  // ------------------- add a user ----------------------------
+  public Map<String, String> showAddUser() {
+    Map<String, String> user = new HashMap<>();
+    System.out.print("First Name: ");
+    user.put("firstname", sc.nextLine());
+    System.out.print("Last Name: ");
+    user.put("lastname", sc.nextLine());
+    System.out.print("Email: ");
+    user.put("email", sc.nextLine());
+    System.out.print("Username: ");
+    user.put("username", sc.nextLine());
+    System.out.print("Password: ");
+    user.put("password", sc.nextLine());
+
+    return user;
+  }
+
+  // ------------------- update a user ----------------------------
+  public Map<String, String> showUpdateUser(Map<String, String> user) {
+    Map<String, String> returnUser = new HashMap<>();
+    sc.nextLine();
+    System.out.println("\nEnter updated user details (leave blank to keep current):");
+    System.out.print("First Name (" + user.get("firstname") + "): ");
+    String firstname = sc.nextLine();
+    System.out.print("Last Name (" + user.get("lastname") + "): ");
+    String lastname = sc.nextLine();
+    System.out.print("Email (" + user.get("email") + "): ");
+    String email = sc.nextLine();
+    System.out.print("Username(" + user.get("username") + "): ");
+    String username = sc.nextLine();
+    System.out.print("Password(" + user.get("password") + "): ");
+    String password = sc.nextLine();
+
+    returnUser.put("firstname", firstname.isEmpty() ? user.get("firstname") : firstname);
+    returnUser.put("lastname", lastname.isEmpty() ? user.get("lastname") : lastname);
+    returnUser.put("email", email.isEmpty() ? user.get("email") : email);
+    returnUser.put("username", username.isEmpty() ? user.get("username") : username);
+    returnUser.put("password", password.isEmpty() ? user.get("password") : password);
+
+    return returnUser;
+  }
+
+  public boolean showDeleteUser(Map<String, String> user) {
+    showUser(user);
+    String yesNo = getStringInput("Are you sure you want to delete this user?\n(type 'yes' or 'no'): ");
+
+    return yesNo == "yes" ? true : false;
+  }
+
+  // ----------------- show "user not added" ------------------
+  public void showUserNotAdded() {
+    System.out.println(tc.RED + tc.BLACK_BG + "Error: User not added" + tc.RESET);
+  }
+
+  // ------------- show all users -----------------
+  public void showAllUsers(List<Map<String, String>> users) {
+    for (Map<String, String> user : users) {
+      System.out.println("First Name: " + user.get("firstname"));
+      System.out.println("Last Name: " + user.get("lastname"));
+      System.out.println("Email: " + user.get("email"));
+      System.out.println("Username: " + user.get("username"));
+      String score = (user.get("quizScore").equals("-1")) ? "N/A" : user.get("quizScore");
+      System.out.println("Score: " + score);
+      String quizDate = (user.get("quizDate") == null) ? "N/A" : user.get("quizScore");
+      System.out.println("Quiz Date Taken: " + quizDate);
+      System.out.println("--------------------------------------------------------------");
     }
+    pressToContinue();
+  }
 
-    // --------- admin methods ----------------
-    public String enterAdminUsername() {
-        return getStringInput("Enter admin username: ");
-    }
+  // ----------- search a single user ------------------------------
+  public int showSearchBy() {
+    System.out.println("\nSearch User By:");
+    System.out.println("1. ID");
+    System.out.println("2. Username");
+    System.out.println("3. Email");
+    System.out.println("4. Exit");
+    return getIntInput("Enter choice: ");
+  }
 
-    public String getAdminPassword() {
-        return getStringInput("Enter admin password: ");
-    }
+  // ----------------- search user by id --------------------------
+  public int inputUserID() {
+    System.out.print("Enter user id: ");
+    return sc.nextInt();
+  }
 
-    // -------------- admin menu ------------------------
-    public int showAdminMenu() {
-        System.out.println("\nAdmin Menu:");
-        System.out.println("1. Manage Users");
-        System.out.println("2. Mange quiz");
-        System.out.println("3. Exit");
-        return getIntInput("Enter choice: ");
-    }
+  // ----------------- search user by username--------------------------
+  public String inputUsername() {
+    System.out.print("Enter username: ");
+    return sc.nextLine();
+  }
 
-    // ----------- User management ----------------
-    public int showManageUserMenu() {
-        System.out.println("\nManage User:");
-        System.out.println("1. Add User");
-        System.out.println("2. View All Users");
-        System.out.println("3. View a User");
-        System.out.println("4. Update User");
-        System.out.println("5. Delete User");
-        System.out.println("6. Exit");
-        return getIntInput("Enter choice: ");
-    }
+  // ----------------- search user by email --------------------------
+  public String inputUserEmail() {
+    System.out.print("Enter email: ");
+    return sc.nextLine();
+  }
 
-    // ------------- input username and last name user -----------------
-    public void inputUserInfo() {
-        String firstname = getStringInput("Enter first name: ");
-        String lastname = getStringInput("Enter last name: ");
-    }
+  public int inputUserChoice() {
+    return getIntInput("Enter choice: ");
+  }
 
-    // ------------------- add a user ----------------------------
-    public Map<String, String> showAddUser() {
-        Map<String, String> user = new HashMap<>();
-        System.out.print("First Name: ");
-        user.put("firstname", sc.nextLine());
-        System.out.print("Last Name: ");
-        user.put("lastname", sc.nextLine());
-        System.out.print("Email: ");
-        user.put("email", sc.nextLine());
-        System.out.print("Username: ");
-        user.put("username", sc.nextLine());
-        System.out.print("Password: ");
-        user.put("password", sc.nextLine());
+  // ----------------- NO user found --------------------------
+  public void showNoUserFound() {
+    System.out.println("No user found");
+  }
 
-        return user;
-    }
+  // ----------------- Invalid Username/Email --------------------------
+  public void showInvalidUsernameEmail() {
+    System.out.println(tc.RED + tc.BLACK_BG + "Invalid email or password" + tc.RESET);
+  }
 
-    // ------------------- update a user ----------------------------
-    public Map<String, String> showUpdateUser(Map<String, String> user ) {
-        Map<String, String> returnUser = new HashMap<>();
-        sc.nextLine();
-        System.out.println("\nEnter updated user details (leave blank to keep current):");
-        System.out.print("First Name (" + user.get("firstname") + "): ");
-        String firstname = sc.nextLine();
-        System.out.print("Last Name (" + user.get("lastname") + "): ");
-        String lastname = sc.nextLine();
-        System.out.print("Email (" + user.get("email") + "): ");
-        String email = sc.nextLine();
-        System.out.print("Username(" + user.get("username") + "): ");
-        String username = sc.nextLine();
-        System.out.print("Password(" + user.get("password") + "): ");
-        String password = sc.nextLine();
+  // ----------------- user found --------------------------
+  public void showUser(Map<String, String> user) {
+    System.out.println("\n--------------------------------------------------------------");
+    System.out.println("First Name: " + user.get("firstname"));
+    System.out.println("Last Name: " + user.get("lastname"));
+    System.out.println("Email: " + user.get("email"));
+    System.out.println("Username: " + user.get("username"));
+    System.out.println("Score: " + user.get("quizScore"));
+    System.out.println("Quiz Date Taken: " + user.get("quizDate"));
+    System.out.println("--------------------------------------------------------------");
+  }
 
-        returnUser.put("firstname", firstname.isEmpty()?user.get("firstname"):firstname);
-        returnUser.put("lastname", lastname.isEmpty()?user.get("lastname"):lastname);
-        returnUser.put("email", email.isEmpty()?user.get("email"):email);
-        returnUser.put("username", username.isEmpty()?user.get("username"):username);
-        returnUser.put("password", password.isEmpty()?user.get("password"):password);
+  // ----------- helper methods ----------------
+  private String getStringInput(String prompt) {
+    System.out.print(prompt);
+    return sc.nextLine();
+  }
 
-        return returnUser;
-    }
+  private int getIntInput(String prompt) {
+    System.out.print("\n" + prompt + "\n> ");
+    int input = sc.nextInt();
+    sc.nextLine();
 
-    public boolean showDeleteUser(Map<String, String> user){
-       showUser(user);
-       String yesNo = getStringInput("Are you sure you want to delete this user?\n(type 'yes' or 'no'): ");
+    return input;
+  }
 
-       return yesNo == "yes" ? true : false;
-    }
-    // ----------------- show "user not added" ------------------
-    public void showUserNotAdded() {
-        System.out.println(tc.RED + tc.BLACK_BG + "Error: User not added" + tc.RESET);
-    }
+  private void pressToContinue() {
+    System.out.print("\nPress 'enter' to continue");
+    sc.nextLine();
+  }
 
-    // ----------------- show "user added" ------------------
-    public void showUserAdded() {
-        System.out.println("Success: User added");
-    }
+  // ----------------------- Success and Fail message ------------------------
+  public void showError(String errorMessage) {
+    System.out.println("\n⚠️" + errorMessage);
+    pressToContinue();
+  }
 
-    // ------------- show all users -----------------
-    public void showAllUsers(List<Map<String, String>> users) {
-        for (Map<String, String> user : users) {
-            System.out.println("First Name: " + user.get("firstname"));
-            System.out.println("Last Name: " + user.get("lastname"));
-            System.out.println("Email: " + user.get("email"));
-            System.out.println("Username: " + user.get("username"));
-            System.out.println("Score: " + user.get("quizScore"));
-            System.out.println("Quiz Date Taken: " + user.get("quizDate"));
-            System.out.println("--------------------------------------------------------------");
-        }
-    }
+  public void showSuccess(String successMessage) {
+    System.out.println("\n🎉" + successMessage);
+    pressToContinue();
+  }
 
-    // ----------- search a single user ------------------------------
-    public int showSearchBy() {
-        System.out.println("\nSearch User By:");
-        System.out.println("1. ID");
-        System.out.println("2. Username");
-        System.out.println("3. Email");
-        System.out.println("4. Exit");
-        return getIntInput("Enter choice: ");
-    }
-
-    // ----------------- search user by id --------------------------
-    public int inputUserID() {
-        System.out.print("Enter user id: ");
-        return sc.nextInt();
-    }
-
-    // ----------------- search user by username--------------------------
-    public String inputUsername() {
-        System.out.print("Enter username: ");
-        return sc.nextLine();
-    }
-
-    // ----------------- search user by email --------------------------
-    public String inputUserEmail() {
-        System.out.print("Enter email: ");
-        return sc.nextLine();
-    }
-
-    public int inputUserChoice() {
-        return getIntInput("Enter choice: ");
-    }
-
-    // ----------------- NO user found --------------------------
-    public void showNoUserFound() {
-        System.out.println("No user found");
-        pressToContinue();
-    }
-
-    // ----------------- Invalid Username/Email --------------------------
-    public void showInvalidUsernameEmail() {
-        System.out.println(tc.RED + tc.BLACK_BG + "Invalid email or password" + tc.RESET);
-        pressToContinue();
-    }
-
-    // ----------------- user found --------------------------
-    public void showUser(Map<String, String> user) {
-        System.out.println("\n--------------------------------------------------------------");
-        System.out.println("First Name: " + user.get("firstname"));
-        System.out.println("Last Name: " + user.get("lastname"));
-        System.out.println("Email: " + user.get("email"));
-        System.out.println("Username: " + user.get("username"));
-        System.out.println("Score: " + user.get("quizScore"));
-        System.out.println("Quiz Date Taken: " + user.get("quizDate"));
-        System.out.println("--------------------------------------------------------------");
-    }
-
-    // ----------------------- Success and Fail message ------------------------
-    public void showSuccessMessage(String message){
-        System.out.println(message);
-    }
-
-    public void showFailMessage(String message){
-        System.out.println(message);
-    }
-
-    // ----------- helper methods ----------------
-    private String getStringInput(String prompt) {
-        System.out.print(prompt);
-        return sc.nextLine();
-    }
-
-    private int getIntInput(String prompt) {
-        System.out.print(prompt);
-        int input = sc.nextInt();
-        sc.nextLine();
-
-        return input;
-    }
-
-    private void pressToContinue() {
-        System.out.print("Press 'enter' to continue.");
-        sc.nextLine();
-    }
-
+  public char showTryAgain(String s) {
+    System.out.println(s);
+    return sc.next().charAt(0);
+  }
 }
