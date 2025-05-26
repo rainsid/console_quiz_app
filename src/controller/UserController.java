c9aaec8c4c07948dbe2402154d827a1f9716dfae
rainsid <adrianroycidro@gmail.com>
2025-05-26 10:57:43 +0800

    save quiz data

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -31,33 +31,53 @@ public class UserController {
       userView.showError("Password cannot be empty");
       return;
     }
-    String storedPassword = dbManager.getUserPassword(username);
-    if (password.equals(storedPassword)) {
+    ArrayList<String> idAndPassword = dbManager.getUserPassword(username);
+    if (password.equals(idAndPassword.get(1))) {
       userView.showSuccess("User login successful");
-      userMenu();
+      userMenu(idAndPassword.get(0));
     } else {
       userView.showError("Invalid Username or Email");
     }
   }
 
-  private void userMenu() {
+  private void userMenu(String id) {
     while (true) {
       int choice = userView.showUserMenu();
+      if(choice == 3) break;
       switch (choice) {
         case 1:
-          takeQuiz();
+          takeQuiz(id);
           break;
       }
     }
   }
 
-  private void takeQuiz() {
+  private void takeQuiz(String id) {
     ArrayList<Question> questions = quizModel.retrieveAllQuestions();
-    if (questions == null) {
+    if (questions == null || questions.isEmpty()) {
       userView.showError("No questions stored in the database");
       return;
     }
     Map<Integer, String> userAnswers = userView.showQuestions(questions);
+
+    int numberOfItems = questions.size();
+    int counter = 0;
+    int quizScore = 0;
+
+    StringBuilder userAnswersString = new StringBuilder();
+    for (Map.Entry<?, ?> entry : userAnswers.entrySet()) {
+      if (questions.get(counter).getCorrectAnswer().equals(entry.getValue())) {
+        quizScore++;
+      }
+      userAnswersString.append(entry.getValue()).append(",");
+      counter++;
+    }
+    if(dbManager.saveQuizData(id, quizScore, String.valueOf(userAnswersString), numberOfItems)) {
+      userView.showMessage("Your score: " + quizScore + "/" + numberOfItems);
+      userView.showMessage(String.valueOf(userAnswersString));
+    }else{
+      userView.showError("Quiz data not saved!");
+    }
   }
 
 }
