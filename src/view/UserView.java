5695593ca0a9c5e2d208829852b457612f19ee47
rainsid <adrianroycidro@gmail.com>
2025-05-30 22:04:05 +0800

    userview update

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -7,17 +7,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class UserView {
   private Scanner sc = new Scanner(System.in);
 
   // ---------------------- user menu -----------------------------
-  public int showUserMenu() {
+  public int showUserMenu(String username) {
     ConsoleUtil.clearConsole();
     System.out.println(Banner.MAIN_BANNER);
     System.out.println(Banner.TRAINEE_MENU);
+    System.out.println("Hey there, " + username + " 👋\n");
     System.out.println("1. Take Quiz");
-    System.out.println("2. View Result");
+    System.out.println("2. Quiz Attempts");
     System.out.println("3. Exit User");
 
     return getIntInput("Enter your choice");
@@ -79,7 +81,46 @@ public class UserView {
     return userAnswers;
   }
 
-  public void showResult()
+  public void showQuizDone() {
+    ConsoleUtil.clearConsole();
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println(Banner.QUIZ_TIME);
+    System.out.println("\n\n🎉 Your done! Press Enter to view your result");
+    sc.nextLine();
+  }
+
+  public void showResult(int quizScore, int numberOfItems, StringBuilder userAnswerString,
+      ArrayList<Question> questions) {
+    ConsoleUtil.clearConsole();
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println(Banner.QUIZ_RESULT);
+
+    System.out.println("Result: " + quizScore + " of " + numberOfItems + " ("
+        + (double) quizScore / (double) numberOfItems * 100.00 + "%)");
+    pressToContinue();
+  }
+
+  public void showQuizAttempts(ArrayList<Map<String, String>> quiz_attempts) {
+    ConsoleUtil.clearConsole();
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println(Banner.QUIZ_ATTEMPT);
+    int attemptCounter = 0;
+    for (Map<String, String> attempt : quiz_attempts) {
+      System.out.println("-------------------------------------------------------");
+      System.out.println("Attempt Number: " + ++attemptCounter);
+      System.out.println("Attempt ID: " + attempt.get("attempt_id"));
+      System.out.println("Score: " + attempt.get("score") + " of " + attempt.get("total_questions") + " ("
+          + Double.parseDouble(attempt.get("score")) / Double.parseDouble(attempt.get("total_questions")) * 100.00
+          + "%)");
+      System.out.println("Attempt Date: " + attempt.get("attempt_date"));
+      System.out.println("-------------------------------------------------------");
+      // String[] answers = attempt.get("answers_concatenated").split(",");
+      // for (String answer : answers) {
+      // System.out.print(answer + " ");
+      // }
+    }
+    pressToContinue();
+  }
 
   // ----------------- helpers
 
@@ -100,6 +141,22 @@ public class UserView {
     }
 
     return input;
+  }
+
+  public void showTraineeInfo(Map<String, String> trainee) {
+    ConsoleUtil.clearConsole();
+    System.out.println(Banner.MAIN_BANNER);
+    System.out.println(Banner.TRAINEE_INFO);
+    System.out.println("\n--------------------------------------------------------------");
+    System.out.println("Trainee ID: " + trainee.get("id"));
+    System.out.println("First Name: " + trainee.get("firstname"));
+    System.out.println("Last Name: " + trainee.get("lastname"));
+    System.out.println("Email: " + trainee.get("email"));
+    System.out.println("Username: " + trainee.get("username"));
+    System.out.println("Date Account Registered: " + trainee.get("createdAt"));
+    System.out.println("Date Account Updated : " + trainee.get("updatedAt"));
+    System.out.println("--------------------------------------------------------------");
+
   }
 
   private String getStringInput(String prompt) {
